37970a81e9bf20de1c0b9884c27986f0e50f0a88
Minjun Youn <minjun1.y@partner.samsung.com>
2018-09-05 12:01:16 -0500

    added date to note

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,26 @@ var User = require('../config/database')
 /* GET home page. */
 router.get('/', function (req, res, next) {
     console.log(req.user)
-    res.render('index', { title: 'Weekly Work Note', user: req.user });
+    var week = getWeek()
+    var newWeek = [];
+    for (day of week) {
+        day = day.toString().split(" ")
+        day = day.slice(0, 4)
+        newWeek.push(day.toString().replace(/,/g, " "))
+    }
+    console.log(newWeek)
+    res.render('index', { user: req.user, week: newWeek });
 });
+
+function getWeek() {
+    now = new Date()
+    var sunday = new Date(now.setDate(now.getDate() - now.getDay()))
+    var result = [new Date(sunday)];
+    while (sunday.setDate(sunday.getDate() + 1) && sunday.getDay() !== 0) {
+        result.push(new Date(sunday));
+    }
+    return result;
+}
+
 
 module.exports = router;
