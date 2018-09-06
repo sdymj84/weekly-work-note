daf79c42f0323e1537cc2d3e4b72ba9680ebdf20
Minjun Youn <minjun1.y@partner.samsung.com>
2018-09-06 18:03:15 -0500

    save

---
+++ public/javascripts/main.js
@@ -0,0 +1,7 @@
+$("#save").addClass("wall-image1")
+
+$("#wall-images-p").on("click", ()=>{
+    var wallId = this.id
+    console.log("wall id" + wallId)
+    $("#save").removeClass().addClass()
+})
