607ee8f477aefdd377e670b547cb7afeee4b8781
sdymj84 <sdymj84@gmail.com>
2018-09-06 22:33:49 -0500

    working on login rec msg

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,10 @@
 $("#wall-images-p").on("click", "img", function() {
     $("#save").removeClass().addClass(this.id)
 })
+
+function loginRecMsg() {
+    $("#login-rec-msg").addClass("show")
+    setTimeout(function() {
+        $("#login-rec-msg").removeClass()
+    }, 3000)
+}
