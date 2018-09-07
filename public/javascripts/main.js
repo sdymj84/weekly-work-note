9e4feef9150b0d2a6a4578e0280eca569034dc42
Minjun Youn <minjun1.y@partner.samsung.com>
2018-09-07 12:00:05 -0500

    wall, toast msg

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,6 +2,9 @@ $("#wall-images-p").on("click", "img", function() {
     $("#save").removeClass().addClass(this.id)
 })
 
+if ($("#show-login-rec-msg").length) {
+    loginRecMsg()
+}
 function loginRecMsg() {
     $("#login-rec-msg").addClass("show")
     setTimeout(function() {
