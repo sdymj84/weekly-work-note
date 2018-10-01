0d5ec89a725c61472008eacc83cd95e54d440666
Minjun Youn <minjun1.y@partner.samsung.com>
2018-10-01 17:39:43 -0500

    Issue fix - save & show for other weeks too

--- routes/index.js
+++ routes/index.js
@@ -38,24 +38,38 @@ router.get('/', function (req, res, next) {
                     newWeek.push(day.toString().replace(/,/g, " "))
                 }
 
-                Note.find({user:id}).then(myNotes=>{
-                    var arrayNotes = []
-                    for (eachDate of newWeek) {
-                        myNotes.forEach(eachNote=>{
-                            if (eachNote.date === eachDate) {
-                                arrayNotes.push(eachNote || {})
-                                console.log(eachNote.date)
-                            }
+                Note.find({user:id}, (err,myNotes)=>{
+                    let exist = false
+                    myNotes.some(eachNote=>{
+                        if (eachNote.date === newWeek[1]) {
+                            exist = true
+                            return true
+                        }
+                    })
+                    if (exist) {
+                        Note.find({user:id}).then(myNotes=>{
+                            return Promise.all(getArrayNotes(myNotes, newWeek))
+                        }).then(arrayNotes=>{
+                            res.render('index', {
+                                user: req.user,
+                                week: newWeek,
+                                notes: arrayNotes, 
+                                wall, font
+                            })
+                        })
+                    } else {
+                        saveNotes(user, id, [])
+                        Note.find({user:id}).then(myNotes=>{
+                            return Promise.all(getArrayNotes(myNotes, newWeek))
+                        }).then(arrayNotes=>{
+                            res.render('index', {
+                                user: req.user,
+                                week: newWeek,
+                                notes: arrayNotes, 
+                                wall, font
+                            })
                         })
                     }
-                    return Promise.all(arrayNotes)
-                }).then(arrayNotes=>{
-                    res.render('index', {
-                        user: req.user,
-                        week: newWeek,
-                        notes: arrayNotes, 
-                        wall, font
-                    })
                 })
             }
         })
@@ -71,44 +85,59 @@ router.get('/', function (req, res, next) {
 router.post('/save', (req, res) => {
     var id = mongoose.Types.ObjectId(req.user.id)
     var weekNotes = []
-    console.log(req.body.tue)
     weekNotes.push(req.body.mon)
     weekNotes.push(req.body.tue)
     weekNotes.push(req.body.wed)
     weekNotes.push(req.body.thu)
     weekNotes.push(req.body.fri)
     weekNotes.push(req.body.etc)
-    // var d = new Date(Date.now())
-    console.log(weekNotes)
-    var week = []
-
+    
     User.findOne({_id:id}, (err,user)=>{
         if (err) console.log(err)
         else {
-            // set date of each date for this week
-            d = new Date(user.currentWeekMon)
-            
-            for (let i=0 ; i<6 ; i++) {
-                week[i] = d.toString().substring(0,15)
-                d.setDate(d.getDate() + 1)
-                
-                Note.updateOne({
-                    user: id,
-                    date: week[i]
-                }, 
-                {
-                    $set: {
-                        note: weekNotes[i],
-                    }
-                },
-                { upsert: true }, (err, dbRes) => {
-                    if (err) console.log(err)
-                })
-            }
+            saveNotes(user, id, weekNotes)
             res.redirect('/')
         }
     })
 })
+
+function getArrayNotes(myNotes, newWeek) {
+    var arrayNotes = []
+    for (eachDate of newWeek) {
+        myNotes.some(eachNote=>{
+            if (eachNote.date === eachDate) {
+                arrayNotes.push(eachNote || {})
+                return true
+            }
+        })
+    }
+    return arrayNotes
+}
+
+// Receive user db result and weekNotes array
+function saveNotes(user, id, weekNotes) {
+    var week = []
+    // set date of each date for this week
+    d = new Date(user.currentWeekMon)
+            
+    for (let i=0 ; i<6 ; i++) {
+        week[i] = d.toString().substring(0,15)
+        d.setDate(d.getDate() + 1)
+        
+        Note.updateOne({
+            user: id,
+            date: week[i]
+        }, 
+        {
+            $set: {
+                note: weekNotes[i] || "",
+            }
+        },
+        { upsert: true }, (err, dbRes) => {
+            if (err) console.log(err)
+        })
+    }
+}
 
 function getWeek(d) {
     // show the week. if Sunday, show previous week
